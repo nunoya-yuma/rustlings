0c2d85de12115f91dedc23facd2cac0e019f262f
nunoya-yuma <nuno.yu.3838@gmail.com>
2026-09-03 01:00:33 +0900

    feat: implement `calculate_price_of_apples` function and add tests for pricing logic

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,10 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(apples: u32) -> u32 {
+    if apples > 40 { apples } else { apples * 2 }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
